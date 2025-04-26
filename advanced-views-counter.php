ecebf3e424cd9bd758d068641cabab29782b380a
schorpy <87610109+schorpy@users.noreply.github.com>
2025-04-26 10:23:19 +0700

    update rest url

--- advanced-views-counter.php
+++ advanced-views-counter.php
@@ -36,7 +36,7 @@ define('ADVICO_PLUGIN_FILE', __FILE__);
 define('ADVICO_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ADVICO_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ADVICO_PLUGIN_ASSETS_URL', ADVICO_PLUGIN_URL . '/assets');
-define('ADVICO_ROUTE_PREFIX', 'avc/v1');
+define('ADVICO_ROUTE_PREFIX', 'advico/v1');
 // define('ADVICO_DEV_MODE', true); // or false for production
 // load composer
 require_once __DIR__ . '/vendor/autoload.php';
